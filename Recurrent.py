ab6df7399035cef930340e65fab27ab1f1d6a483
Batool55 <60565928+Batool55@users.noreply.github.com>
2020-08-31 17:51:32 +0300

    Create Recurrent.py

---
+++ Recurrent.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv('all_features.csv')
+data['Gender'] = data['Gender'].replace(['male','female'],[0,1])
+X = data.iloc[:,1:13].values
+y = data.iloc[:,13].values
+x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.3, shuffle=True, random_state=0)
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.fit_transform(x_test)
+
+x_train = torch.FloatTensor(x_train)
+x_test = torch.FloatTensor(x_test)
+y_train = torch.FloatTensor(y_train)
+y_test = torch.FloatTensor(y_test)
+
+class RecurrentNN(nn.Module):
+    def __init__(self ):
+        super(RecurrentNN, self).__init__()
+        self.rnn = nn.RNN(12,300,3,batch_first = True,nonlinearity = 'tanh')
+        self.fc = nn.Linear(300,1)
+        
+    def forward(self, x):
+        #h0 = Variable(torch.zeros(3, x.size(0), 300))
+        out,hn = self.rnn(x)
+        out = F.sigmoid(self.fc(out))
+        return out
+    
+model = RecurrentNN()
+criterion = torch.nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=.1)
+        
+model.train()
+epochs = 100
+errors = []
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    x_train  = Variable(x_train.view(-1, 3169, 12))
+    y_train = Variable(y_train )
+    y_pred = model(x_train)
+    loss = criterion(y_pred.squeeze(), y_train)
+    errors.append(loss.item())
+    print('Epoch {}: train loss: {}'.format(epoch, loss.item()))
+    loss.backward()
+    optimizer.step()
+    
+model.eval()
+x_test  = Variable(x_test.view(-1, 1359, 12))
+y_pred = model(x_test)
+after_train = criterion(y_pred.squeeze(), y_test)
+print('Test loss after Training' , after_train.item())
+preds = y_pred.detach().numpy()
+preds = preds.round()
+preds = preds.transpose(2,0,1).reshape(-1,)
+accuracy_score(y_test,preds)
+   
